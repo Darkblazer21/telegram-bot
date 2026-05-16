daca88022e90d426fe93fa64ebad2bea0a4fc04c
Darkblazer21 <128042259+Darkblazer21@users.noreply.github.com>
2026-05-16 17:59:48 +0000

    Add comments in main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-import requests
-import json 
+import json
 from pathlib import Path
 
+import requests
 
 currencies = ("USD", "EUR", "XOF")
-BASE_URL = "https://api.frankfurter.dev"
-RATES_ENDPOINT = BASE_URL + "/v2/rates"
+BASE_URL = "https://api.frankfurter.dev"  # Base URL to the Frankfurter API
+RATES_ENDPOINT = BASE_URL + "/v2/rates"  # Rates endpoint to the Frankfurter API
 
-r = requests.get(RATES_ENDPOINT)
+r = requests.get(RATES_ENDPOINT)  # Store the rates response in the variable
 
 
-contents = r.json()
-list_of_contents = list(contents)
+contents = r.json()  # Converts the response in Json format
+list_of_contents = list(contents)  # Converts the response into a list
 
-path = Path("exchange_rates.txt")
+path = Path("exchange_rates.txt")  # The path to the exchange rates file
 
+# Check if the path exists
 if not path.exists():
-    data = json.dumps(list_of_contents)
-    path.write_text(data)
+    data = json.dumps(list_of_contents)  # Store the data in Json format
+    path.write_text(data)  # Write the data content in the file
